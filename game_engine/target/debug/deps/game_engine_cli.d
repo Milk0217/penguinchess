79d3d0e75a77f72b7d4d15a25570dedaf6a34530
Milky0217 <m943790568@163.com>
2026-05-09 10:13:08 +0800

    cleanup: untrack build artifacts and old model locations

--- game_engine/target/debug/deps/game_engine_cli.d
+++
@@ -1,5 +0,0 @@
-E:\programming\penguinchess\game_engine\target\debug\deps\game_engine_cli.d: src\bin\cli.rs
-
-E:\programming\penguinchess\game_engine\target\debug\deps\game_engine_cli.exe: src\bin\cli.rs
-
-src\bin\cli.rs:
